c1f366849d5be91c8425db4a7613827b1cb5bcb3
Gorbunov Klim <lore-05@mail.ru>
2023-11-13 11:51:23 +0300

    Create 2.cpp

---
+++ 1/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long double a;
+    long double b;
+    cin >> a >> b;
+
+    double c;
+    c = a/b;
+    cout << a+b << " " << a-b << " " << a*b << " " << c;
+    return 0;
+}
+
+
